a60e81db749fb69656b12bdd218bd3b6f1ad68a4
unknown <you@example.com>
2023-04-19 21:27:59 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,219 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-lobby-",
+      "name": "LOBBY ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.5983592786440379,
+        "pitch": -0.01708574237110483,
+        "fov": 1.3934346732050444
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4647852292398236,
+          "pitch": 0.07615231191159566,
+          "rotation": 4.71238898038469,
+          "target": "1-master-bedroom-"
+        },
+        {
+          "yaw": -0.005655861579404231,
+          "pitch": 0.12839054408900807,
+          "rotation": 0,
+          "target": "2-bedroom-"
+        },
+        {
+          "yaw": 1.9499066480330427,
+          "pitch": 0.06233935660161016,
+          "rotation": 1.5707963267948966,
+          "target": "3-gym"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-master-bedroom-",
+      "name": "MASTER BEDROOM ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.9990806960038618,
+        "pitch": 0.18774620470679082,
+        "fov": 1.3934346732050444
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6874394401526782,
+          "pitch": 0.09526254809509282,
+          "rotation": 0,
+          "target": "0-lobby-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bedroom-",
+      "name": "BEDROOM ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09507474618902023,
+          "pitch": 0.08909571215292544,
+          "rotation": 0,
+          "target": "0-lobby-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-gym",
+      "name": "GYM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.4183943994230237,
+        "pitch": 0.020658889089968824,
+        "fov": 1.3934346732050444
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09975042249704202,
+          "pitch": 0.06258093215702587,
+          "rotation": 0,
+          "target": "0-lobby-"
+        },
+        {
+          "yaw": -0.9832800219851485,
+          "pitch": 0.05138696658384312,
+          "rotation": 0,
+          "target": "4-spa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-spa",
+      "name": "SPA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7605168785636991,
+          "pitch": 0.17007668330126258,
+          "rotation": 0.7853981633974483,
+          "target": "3-gym"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Second Floor",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
